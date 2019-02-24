d61fbbc01b16ca6803a0d58ec87a3fdaf37315b7
Rafael Pinto <santospinto.rafael@gmail.com>
2019-02-24 19:39:47 +0000

    Added data tier

---
+++ data/createContactsTable.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk");
+AWS.config.update({
+    region: "eu-central-1",
+    endpoint: process.env.DB_ENDPOINT
+});
+
+const dynamodb = new AWS.DynamoDB();
+const params = {
+    TableName: "Contacts",
+    KeySchema: [
+        { AttributeName: "email", KeyType: "HASH" },
+        { AttributeName: "timestamp", KeyType: "RANGE" }
+    ],
+    AttributeDefinitions: [
+        { AttributeName: "email", AttributeType: "S" },
+        { AttributeName: "timestamp", AttributeType: "N" }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5
+    }
+};
+
+dynamodb.createTable(params, function (err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
